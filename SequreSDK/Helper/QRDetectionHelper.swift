b7558a4c90bf16dd6c6dbd8d875096c1cb38cb61
Aaron Evanjulio <aaronevanjulio18@gmail.com>
2024-10-11 14:24:50 +0700

    feat: test 19

--- SequreSDK/Helper/QRDetectionHelper.swift
+++ SequreSDK/Helper/QRDetectionHelper.swift
@@ -14,7 +14,7 @@ public struct Result {
 
 public class QRDetectionHelper: NSObject {
     private var objectDetector: ObjectDetector?
-    
+
     override public init() {
         super.init()
 
@@ -28,26 +28,30 @@ public class QRDetectionHelper: NSObject {
             objectDetector = try ObjectDetector.detector(options: option)
         } catch {
             print("Failed to initialize ObjectDetector: \(error.localizedDescription)")
-            objectDetector = nil 
+            objectDetector = nil
         }
     }
-    
+
     public func detectObject(frame pixelBuffer: CVPixelBuffer) -> Result? {
         if objectDetector == nil { return nil }
         guard let mlImage = MLImage(pixelBuffer: pixelBuffer) else { return nil }
         do {
             let startDate = Date()
-            let detectionResult = try objectDetector!.detect(mlImage: mlImage)
+            let detectionResult = try objectDetector?.detect(mlImage: mlImage)
             let interval = Date().timeIntervalSince(startDate) * 100
-            
-            let filteredDetectionResult = detectionResult.detections.filter { detection in
+
+            let filteredDetectionResult = detectionResult?.detections.filter { detection in
                 detection.categories.first!.score > 0.115
             }
-            
-            return Result(inferenceTime: interval, detections: filteredDetectionResult)
-            
+
+            if let result = filteredDetectionResult {
+                return Result(inferenceTime: interval, detections: result)
+            } else {
+                return nil
+            }
+
         } catch {
-             print("Failed to detect: \(error.localizedDescription)")
+            print("Failed to detect: \(error.localizedDescription)")
             return nil
         }
     }
