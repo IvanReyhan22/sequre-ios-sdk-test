370db15b2e34eb34777b806198f670f487568690
Ahmad Ivan Reyhan <ivanreyhan2002@gmail.com>
2024-10-17 14:33:05 +0700

    feat(model): update tflite model

--- SequreSDK/Helper/QRDetectionHelper.swift
+++ SequreSDK/Helper/QRDetectionHelper.swift
@@ -14,17 +14,18 @@ public struct Result {
 
 public class QRDetectionHelper: NSObject {
     private var objectDetector: ObjectDetector?
-
+    
     override public init() {
         super.init()
-
-//        guard let modelPath = Bundle.main.path(forResource: "sequre-combine", ofType: "tflite") else {
-        guard let modelPath = Bundle.main.path(forResource: "sequre-v2-od", ofType: "tflite") else {
+        
+        //        guard let modelPath = Bundle.main.path(forResource: "sequre-combine", ofType: "tflite") else {
+        //        guard let modelPath = Bundle.main.path(forResource: "sequre-v2-od", ofType: "tflite") else {
+        guard let modelPath = Bundle.main.path(forResource: "sequre-combine-v2", ofType: "tflite") else {
             fatalError("Failed to load the model file")
         }
-
+        
         let option = ObjectDetectorOptions(modelPath: modelPath)
-
+        
         do {
             objectDetector = try ObjectDetector.detector(options: option)
         } catch {
@@ -32,7 +33,7 @@ public class QRDetectionHelper: NSObject {
             objectDetector = nil
         }
     }
-
+    
     public func detectObject(frame pixelBuffer: CVPixelBuffer) -> Result? {
         if objectDetector == nil { return nil }
         guard let mlImage = MLImage(pixelBuffer: pixelBuffer) else { return nil }
@@ -40,17 +41,17 @@ public class QRDetectionHelper: NSObject {
             let startDate = Date()
             let detectionResult = try objectDetector?.detect(mlImage: mlImage)
             let interval = Date().timeIntervalSince(startDate) * 100
-
+            
             let filteredDetectionResult = detectionResult?.detections.filter { detection in
                 detection.categories.first!.score > 0.115
             }
-
+            
             if let result = filteredDetectionResult {
                 return Result(inferenceTime: interval, detections: result)
             } else {
                 return nil
             }
-
+            
         } catch {
             print("Failed to detect: \(error.localizedDescription)")
             return nil
